eb6deddb950946b608bbbe2d05ba01811edfe99c
arminyahya <arminyahyaa@gmail.com>
2025-06-12 17:03:11 +0330

    chore: initial payment-options created

---
+++ src/components/payment-options-screen/index.tsx
@@ -0,0 +1,18 @@
+import AddNewCardBox from "../add-new-card-box";
+import CardPreview from "../card-preview";
+
+export default function PaymentOptionsScreen() {
+    return (
+        <div className='w-full flex flex-col gap-5'>
+            <div className="flex flex-col gap-3">
+                <div className="text-2xl font-bold text-left">Your Card Info</div>
+                <CardPreview
+                    fullName='Armin Yahya'
+                    cardNumber='4241921472193456'
+                    expirationDate='12/25'
+                />
+                <AddNewCardBox />
+            </div>
+        </div>
+    )
+}
